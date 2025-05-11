ff2f2a47acf0104d9bbad1231516f3a472d42733
z-shubhamAgrawal <shubhama@zenarate.com>
2025-05-11 19:21:26 +0530

    Initial Commit

---
+++ pg_admin/pg_adminApp.swift
@@ -0,0 +1,17 @@
+//
+//  pg_adminApp.swift
+//  pg_admin
+//
+//  Created by Shubham on 11/05/25.
+//
+
+import SwiftUI
+
+@main
+struct pg_adminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pg_adminTests/pg_adminTests.swift
@@ -0,0 +1,17 @@
+//
+//  pg_adminTests.swift
+//  pg_adminTests
+//
+//  Created by Shubham on 11/05/25.
+//
+
+import Testing
+@testable import pg_admin
+
+struct pg_adminTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
